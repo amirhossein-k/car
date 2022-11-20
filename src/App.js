ab2bd58ca98fc56c2c410de4351ff8cdb30ac5ad
amirhossein karimi <haamir3030@gmail.com>
2022-11-20 18:33:42 +0000

    Update 9 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import styled from "styled-components";
-
+import Navbars from "./components/Navbars";
 
 const App = () => {
-  return <Container></Container>;
+  return (
+    <Container fluid>
+      <Navbars />
+    </Container>
+  );
 };
 
 export default App;
