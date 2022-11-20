0161374d957f2cc74af81d00ac3cb7cbb86d2c8f
amirhossein karimi <haamir3030@gmail.com>
2022-11-20 16:47:08 +0000

    Update 10 files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
-import CarsShow from './components/CarsShow'
-import Info from './components/Info'
-import Navbar from './components/Navbar'
-import Rowcar from './components/Rowcar'
-import SerchBox from './components/SerchBox'
-import Swipper from './components/Swipper'
-
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
 
 
 const App = () => {
-  return (
+  return <Container></Container>;
+};
 
-      <MainBody>
-        <Navbar/>
-        <Swipper/>
-        <SerchBox/>
-        <Rowcar/>
-        <CarsShow/>
-        <Info/>
-      </MainBody>
-    
-    
-  )
-}
-const MainBody = styled.section`
-width: 100%;
-min-height: 100vh;
-scroll-snap-type: y mandatory;
-position: relative;
-
-`;
-export default App
+export default App;
