5b4ffed07bf72fa10cbd65bbf5bb90b53fb86bb7
amirhossein karimi <haamir3030@gmail.com>
2022-11-22 20:07:35 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import styled from "styled-components";
 import Navbars from "./components/Navbars";
+import Swipper from "./components/Swipper";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   return (
     <Container fluid>
       <Navbars />
+      {/* <Swipper /> */}
+      <SearchBox />
     </Container>
   );
 };
